94065e3f036ac10adfa110e432b881b1cd3d958c
vidhi-am <66794454+vidhi-am@users.noreply.github.com>
2022-09-09 12:31:26 +0530

    code and files

---
+++ writePassengerData.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("<your influx db token>")
+org = "<your influx db org name>"
+url = "<your influx db custom url>"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+df = pd.read_csv('AirPassengers.csv')
+for i in df.index:
+    point = (
+    Point("passengers")
+    .tag("month", df.iloc[i,0])
+    .field("passengers", df.iloc[i,1])
+  )
+    write_api.write(bucket=bucket, org=org, record=point)
+    time.sleep(1) # separate points by 1 second
+
+
